05addb71a4bc0adca44a0714d515cf0bd9481f0d
zonde306 <13820323+zonde306@users.noreply.github.com>
2022-07-21 13:43:23 +0800

    init

---
+++ Main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.IL2CPP;
+using HarmonyLib;
+
+namespace RF5_RF4_Chara
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+	[BepInProcess(GAME_PROCESS)]
+    public class Main : BasePlugin
+    {
+        #region PluginInfo
+        private const string GUID = "8F4D47FB-3DED-F400-04CD-181FE5F14785";
+        private const string NAME = "RF5_RF4_Chara";
+        private const string VERSION = "1.0";
+        private const string GAME_PROCESS = "Rune Factory 5.exe";
+        #endregion
+
+        public override void Load()
+        {
+            new Harmony(GUID).PatchAll();
+        }
+    }
+}
---
+++ SystemDataPatch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using BepInEx;
+
+namespace RF5_RF4_Chara
+{
+	[HarmonyPatch(typeof(SaveData.RF5SystemData), nameof(SaveData.RF5SystemData.GetSaveFlag))]
+	public class SystemDataPatch
+	{
+		public static bool Prefix(SaveData.RF5SystemData.FLAG id, ref bool __result)
+		{
+			if (id == SaveData.RF5SystemData.FLAG.SaveLink)
+			{
+				__result = true;
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
