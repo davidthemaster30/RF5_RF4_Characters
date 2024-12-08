65c2ee49633ec5de9babb51abec323f31dd0f996
David <davidthemaster30@hotmail.com>
2024-12-08 18:08:22 -0500

    use PluginInfoProps and add loading and loaded logging

--- Main.cs
+++ Main.cs
@@ -3,24 +3,22 @@ using BepInEx.Logging;
 using BepInEx.Unity.IL2CPP;
 using HarmonyLib;
 
-namespace RF5_RF4_Chara
-{
-    [BepInPlugin(GUID, NAME, VERSION)]
-    [BepInProcess(GAME_PROCESS)]
-    public class Main : BasePlugin
-    {
-        #region PluginInfo
-        private const string GUID = "8F4D47FB-3DED-F400-04CD-181FE5F14785";
-        private const string NAME = "RF5_RF4_Chara";
-        private const string VERSION = "1.0";
-        private const string GAME_PROCESS = "Rune Factory 5.exe";
-        #endregion
+namespace RF5_RF4_Chara;
 
-        internal static readonly new ManualLogSource Log = BepInEx.Logging.Logger.CreateLogSource("RF5_RF4_Chara");
-        public override void Load()
-        {
-            Log.LogInfo($"Plugin {NAME} is loaded!");
-            new Harmony(GUID).PatchAll();
-        }
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInProcess(GAME_PROCESS)]
+public class Main : BasePlugin
+{
+    private const string GAME_PROCESS = "Rune Factory 5.exe";
+
+    internal static readonly new ManualLogSource Log = BepInEx.Logging.Logger.CreateLogSource("RF5_RF4_Chara");
+    public override void Load()
+    {
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_NAME} {MyPluginInfo.PLUGIN_VERSION} is loading!");
+
+        new Harmony(GUID).PatchAll();
+
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_NAME} {MyPluginInfo.PLUGIN_VERSION} is loaded!");
     }
 }
+
