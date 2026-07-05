7ad907520703f7c73041e94c94f3e5473b1b0abc
David Tremblay <davidthemaster30@hotmail.com>
2026-07-05 16:14:42 -0400

    Add git release to release script

--- make_release.sh
+++ make_release.sh
@@ -1,6 +1,20 @@
 rm -rf obj
 rm -rf bin
 
-dotnet build RF5_RF4_Chara.csproj -f net6.0 -c Release
+CSPROJ_PATH="RF5_RF4_Chara.csproj"
 
-zip -j 'RF5_RF4_Chara_v1.1.0.zip' './bin/Release/net6.0/RF5_RF4_Chara.dll'
+dotnet build $CSPROJ_PATH -f net6.0 -c Release
+
+VERSION=$(grep -oP '(?<=<Version>)[^<]+' "$CSPROJ_PATH" || true)
+PROJECTNAME=$(grep -oP '(?<=<AssemblyName>)[^<]+' "$CSPROJ_PATH" || true)
+ZIP_NAME="${PROJECTNAME}_v${VERSION}.zip"
+
+zip -j "${ZIP_NAME}" './bin/Release/net6.0/RF5_RF4_Chara.dll'
+
+git tag "v${VERSION}"
+git push origin "v${VERSION}"
+ 
+gh release create "v${VERSION}" "${ZIP_NAME}" \
+  --title "v${VERSION}" \
+  --generate-notes
+  
